996d5355cb608599772269242d6324e2e0417248
yohan-th <ythollet@student.le-101.fr>
2018-05-25 18:45:32 +0200

    isupper

--- Includes/char.h
+++ Includes/char.h
@@ -24,7 +24,7 @@ int		ft_isdigit(int c);
 int		ft_islower(int c);
 int		ft_isprint(int c);
 int		ft_isspace(char c);
-int		ft_isupper(char c);
+int		ft_isupper(int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 int		ft_nbrchar(char *str, char c);
